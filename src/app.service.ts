653dd6582f6769c0a38b9cb4637de68af71c0183
aleksaarsic <aleksa.arsic@htecgroup.com>
2023-04-24 13:39:59 +0200

    Add Kafka producer

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(
+    private readonly configService: ConfigService,
+    @Inject('PRODUCT_SERVICE') private readonly client: ClientProxy,
+  ) {}
   getHello(): string {
     return 'Hello World!';
+  }
+
+  kafkaTest(message) {
+    return this.client.emit(
+      this.configService.get('MESSAGE_PRODUCTS_TOPIC'),
+      message,
+    );
   }
 }
