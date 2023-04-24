1efa8833d2dbf09c841c428ce7d7bc8be6ef8ef0
aleksaarsic <aleksa.arsic@htecgroup.com>
2023-04-24 13:43:54 +0200

    Remove unusable code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,10 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ClientKafka } from '@nestjs/microservices';
 import { randomUUID } from 'crypto';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    @Inject('PRODUCT_SERVICE') private readonly client: ClientKafka,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
