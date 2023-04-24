d333255de7879a09781f9d453aee48198ae889a6
Aleksa Arsić <76953102+arsicaleksa@users.noreply.github.com>
2023-04-24 13:40:29 +0200

    Add Kafka producer (#1)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ClientKafka } from '@nestjs/microservices';
+import { randomUUID } from 'crypto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('PRODUCT_SERVICE') private readonly client: ClientKafka,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('kafka-produce')
+  kafkaProduce() {
+    const uuid = randomUUID();
+    const message = {
+      id: uuid,
+      name: 'Nike Patike',
+      category: 'Patike',
+      brand: 'Nike',
+      description: 'Nike patike za seljake i poljoprivrednike',
+    };
+
+    return this.appService.kafkaTest(message);
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ClientsModule } from '@nestjs/microservices';
+import { MessageConfig } from './message/message.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.registerAsync([
+      {
+        name: 'PRODUCT_SERVICE',
+        useClass: MessageConfig,
+        imports: [ConfigModule],
+      },
+    ]),
+    ConfigModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
